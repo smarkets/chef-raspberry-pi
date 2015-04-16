7dbd1dc38a347099a4cb5318c44286b59b32e25b
Joshua Padgett <jpadgett@chef.io>
2015-04-16 15:26:25 -0700

    Added multiple memory split options added in new kernel Supports new Raspi v2 Bumped version number

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 default["raspberry-pi"]["overscan"] = true
 default["raspberry-pi"]["hdmi_mode"] = nil
+default["raspberry-pi"]["memory_split"] = {
+  "256"  => 64,
+  "512"  => 128,
+  "1024" => 128
+}
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email "russ.garrett@smarkets.com"
 license          "MIT"
 description      "Handles initial configuration of a Raspberry Pi"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
-
+version          "0.2.0"
